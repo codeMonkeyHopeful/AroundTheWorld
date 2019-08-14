89621142922992c38fd9d78056a1b7c5b9ee530f
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-14 17:32:27 -0400

    slight cleanup

--- public/components/Home.js
+++ public/components/Home.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {updateUserIP} from '../Redux/index.js'
-import storetemp from '../Redux/index.js'
 
 class Home extends Component {
 
     storeIP = (event) => {
       event.preventDefault()
       this.props.updateIP(event.target.ip.value)
+      // this.props.history.push('/test')
 
         }
 
@@ -19,7 +19,7 @@ class Home extends Component {
          IP Address: 
                 <input type = "text" name ='ip' id = "ip"  placeholder='Enter Your IP Here' />
          <br />
-                <button type = "submit">Give me your info</button>
+                <button type = "submit">Give Me Your Info</button>
              </form>
             </center>
         </div>
