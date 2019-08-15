91d774a95c3af25aa52bea12cebe1304d8a1f55d
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-15 16:39:20 -0400

    all data pulls working so far

--- public/Redux/index.js
+++ public/Redux/index.js
@@ -7,6 +7,7 @@ import thunk from 'redux-thunk';
 
 const GOT_USER_IP = 'GOT_USER_IP'
 const GOT_USER_LOCATION = 'GOT_USER_LOCATION'
+const GOT_USER_IMAGE = 'GOT_USER_IMAGE'
 
 //action triggers
 
@@ -18,13 +19,18 @@ export const updateUserLocation = (location) =>{
     return {type: GOT_USER_LOCATION, location}
 }
 
+export const updateUserImage = (image) => {
+    return {type: GOT_USER_IMAGE, image}
+}
+
 //create Thunks if needed
 
 //initial state
 
 const intitialState = 
 {ip: 0,
- location: {}
+ location: {},
+ image: ''
 }
 
 //create reducer
@@ -35,6 +41,8 @@ switch(action.type){
         return {...state, ip: action.ip}
     case GOT_USER_LOCATION:
         return {...state, location: action.location}
+    case GOT_USER_IMAGE:
+        return {...state, image: action.image}
     default:
         return state
 }
--- public/components/Dashboard.js
+++ public/components/Dashboard.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Axios from 'axios';
-import {updateUserLocation} from '../Redux/index'
+import {updateUserLocation, updateUserImage} from '../Redux/index'
+
 
 
 class Dashboard extends Component {
 
+    //Pull IP info
     ipInfo = Axios.get('/api1')
     .then(res=> Axios.get(`http://api.ipstack.com/${this.props.ip}?access_key=${res.data}`))
     .then(info=> this.props.updateLocationInfo(info.data))
-
-    imageInfo = Axios.get(`http://api.ipstack.com/${this.props.ip}?access_key=fafbee55e815cc083f5391158fcc3ddf`)
-    .then(res => this.props.updateLocationInfo(res.data))
+    .then(Axios.get('/api2'))
+    .then(res => Axios.get(`https://api.nasa.gov/planetary/earth/imagery/?lon=${this.props.location.longitude}&lat=${this.props.location.latitude}&date=2016-08-01&cloud_score=False&api_key=DEMO_KEY`))
+    //need to look into why /api2 isn't working
+    .then(info=> this.props.updateImageInfo(info.data))
     .catch(e=> console.log(e))
+
+
+    
+    //Pull NASA info
+    // ipInfo = 
+    // .then(res=> Axios.get(`https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2016-08-01&cloud_score=False&api_key=DEMO_KEY`))
+    // .then(info=> this.props.updateImageInfo(info.data))
 
   render() {
     return (
@@ -27,13 +37,16 @@ class Dashboard extends Component {
 
 const mapDispatch = dispatch => (
   {
-    updateLocationInfo: (location) => dispatch(updateUserLocation(location))
+    updateLocationInfo: (location) => dispatch(updateUserLocation(location)),
+    updateImageInfo: (image) => dispatch(updateUserImage(image)) 
+
 }
 );
 
 const mapState= state => ({
     ip: state.ip,
-    location: state.location
+    location: state.location,
+    image: state.image
 });
 
 export default connect(
