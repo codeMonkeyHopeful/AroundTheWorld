987ee4a92531b7e0267eafcb619de9d868a90d3c
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-15 14:39:50 -0400

    added back end api routes to hold keys

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-console.log(process.env.IP_STACK_API)
+//console.log(process.env.IP_STACK_API)
 
 const app = express();
 const PORT = process.env.PORT;
@@ -13,6 +13,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/components/index.html'));
 });
+
 
 app.listen(PORT, () => {
   console.log(
